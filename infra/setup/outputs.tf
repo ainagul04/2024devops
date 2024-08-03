21f0f9c27141bdac5844e47ef089837f013eab36
Ainagul Takanbekova <ainagultakanbekova@Ainaguls-MacBook-Pro.local>
2024-08-03 10:10:54 -0500

    terraform codes

---
+++ infra/setup/outputs.tf
@@ -0,0 +1,12 @@
+output "cd_user_access_key_id" {
+    description = "aws key id for cd user"
+    value = aws_iam_access_key.cd.id
+  
+}
+
+output "cd_user_access_key_secret" {
+    description = "Access key secret for cd user"
+    value = aws_iam_access_key.cd.secret
+    sensitive = true
+  
+}
